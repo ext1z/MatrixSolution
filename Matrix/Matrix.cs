c7e1cf88e3dfdf15d5f538ff66d47f941595bbe6
ext1z <ext1z1tabgod@gmail.com>
2025-08-06 14:09:16 +0500

    OOP principle

---
+++ Matrix/Matrix.cs
@@ -0,0 +1,115 @@
+﻿using System.Data;
+
+namespace Matrix;
+
+public class Matrix
+{
+    public int Rows { get; }
+    public int Columns { get; }
+    private readonly int[,] _data;
+
+
+    public Matrix(int[,] data)
+    {
+        if (data == null &&
+            data.GetLength(0) <= 0 &&
+            data.GetLength(1) <= 0)
+            throw new ArgumentNullException(nameof(data), "Matrix null bolish kerak emas");
+
+        Rows = data.GetLength(0);
+        Columns = data.GetLength(1);
+        _data = data;
+    }
+
+
+    /// <summary>
+    /// Indexator orqali matrixni(arrayni) qiymatlarini korish. Matrixni ozgartirmagan xolatda
+    /// </summary>
+    public int this[int row, int col]
+    {
+        get => _data[row, col];
+        set => _data[row, col] = value;
+    }
+
+    /// <summary>
+    /// B) Matrixni sortirovka qilish
+    /// </summary>
+    public void SortArray()
+    {
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+            {
+                for (int k = j + 1; k < Columns; k++)
+                {
+                    if (_data[i, j] > _data[i, k])
+                    {
+                        int temp = _data[i, j];
+                        _data[i, j] = _data[i, k];
+                        _data[i, k] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// C) Matrixani toq elementlarini qiymatlari summasini topish;
+    /// </summary>
+    /// <returns>int</returns>
+    public int GetSumOddNumbersOfArray()
+    {
+        int sum = 0;
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+            {
+                if (_data[i, j] % 2 != 0)
+                {
+                    sum += _data[i, j];
+                }
+            }
+        }
+        return sum;
+    }
+
+    /// <summary>
+    /// D) Matrixda takrorlangan elementlar indexlarini massiv ko'rinishida chiqarish;
+    /// </summary>
+    /// <returns></returns>
+    public List<int[]> FindDuplicateIndexes()
+    {
+        Dictionary<int, List<int[]>> dict = new();
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+            {
+                int element = _data[i, j];
+                if (!dict.ContainsKey(element))
+                    dict[element] = new List<int[]>();
+
+                dict[element].Add(new int[] { i, j });
+            }
+        }
+
+        return dict
+            .Where(pair => pair.Value.Count >= 2)
+            .SelectMany(pair => pair.Value)
+            .ToList();
+    }
+
+    /// <summary>
+    /// Matrixni print qilish metodi
+    /// </summary>
+    public void Print()
+    {
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+            {
+                Console.Write($"{_data[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -3,131 +3,30 @@
 
 
 
+Matrix.Matrix matrix = new (new int[,]
+{
+    { 3, 2, 1, 1, 2 },
+    { 6, 5, 4, 4, 1 },
+    { 9, 8, 7, 7, 10 }
+});
 
-int[,] numbers = { { 3, 2, 1 , 1, 2}, { 6, 5, 4 , 4, 1}, {9, 8, 7, 7, 10} };
 
+//matrix.SortArray();
 
+//var duplictates =  matrix.FindDuplicateIndexes();
 
-
-
-//var result = FindDuplicateIndexes(numbers);
-//foreach (var index in result)
+//foreach (var index in duplictates)
 //    Console.WriteLine($"[Row : {index[0]}, Column : {index[1]}]");
 
 
-
-
-//var sortedArray = SortedArray(numbers);
-//var sumOfOddNumbers = TheSumOddNumbersOfArray(numbers);
-
-
-//Testlash uchun
-
-//foreach (var item in sortedArray)
+//for (int i = 0; i < matrix.Rows; i++)
 //{
-//    Console.WriteLine(item);
+//    for (int j = 0; j < matrix.Columns; j++)
+//    {
+//        Console.Write(matrix[i, j] + " ");
+//    }
+//    Console.WriteLine();
 //}
 
-//Console.WriteLine(sumOfOddNumbers);
 
-
-
-#region B) Matrisani sortirovka qilish
-
-int[,] SortedArray(int[,] arr)
-{
-    int rows = arr.GetLength(0);
-    int columns = arr.GetLength(1);
-
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            for (int k = j + 1; k < columns; k++)
-            {
-                if (arr[i, j] > arr[i, k])
-                {
-                    int temp = arr[i, j];
-                    arr[i, j] = arr[i, k];
-                    arr[i, k] = temp;
-                }
-            }
-        }
-    }
-
-    return arr;
-}
-
-#endregion
-
-
-
-#region C) Matricani toq elementlarini qiymatlari summasini topish;
-
-int TheSumOddNumbersOfArray(int[,] arr)
-{
-    int rows = arr.GetLength(0);
-    int columns = arr.GetLength(1);
-
-    int sumOfNumbers = 0;
-
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            if (arr[i, j] % 2 != 0)
-            {
-                sumOfNumbers += arr[i, j];
-            }
-        }
-    }
-
-    return sumOfNumbers;
-}
-
-#endregion
-
-
-
-#region D) Matricada takrorlangan elementlar indexlarini massiv ko'rinishida chiqarish;
-
-
-List<int[]> FindDuplicateIndexes(int[,] arr)
-{
-    int rows = arr.GetLength(0);
-    int columns = arr.GetLength(1);
-    Dictionary<int, List<int[]>> dict = new();
-
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            int element = arr[i, j];
-            if (!dict.ContainsKey(element))
-                dict[element] = new List<int[]>();
-
-            dict[element].Add(new int[] { i, j });
-        }
-    }
-
-    var repeatingIndixesList = dict
-        .Where(pair => pair.Value.Count >= 2)
-        .SelectMany(pair => pair.Value)
-        .ToList();
-
-    return repeatingIndixesList;
-}
-
-
-
-#endregion
-
-
-
-
-//1.A(MxN) matrica(ikki o'lchovli) beriladi. Kiruvchi parametrlar M va N butun sonlar.
-//   Quyidagilarni bajaring:
-//   a) Ixtiyoriy sonlar bilan matricani to'ldirish;
-//   b) Matrisani sortirovka qilish
-//   c) Matricani toq elementlarini qiymatlari summasini topish;
-//   d) Matricada takrorlangan elementlar indexlarini massiv ko'rinishida chiqarish;
+//Console.WriteLine(matrix.GetSumOddNumbersOfArray());
